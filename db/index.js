67b3ecce8d325ef0834d960a920411257d7e38c1
MDS9703 <mitchellstanley3@gmail.com>
2023-04-28 18:07:19 -0400

    second commit

---
+++ db/index.js
@@ -0,0 +1,32 @@
+const { Client } = require("pg");
+const client = new Client("postgres://localhost:5432/juicebox-dev");
+
+async function getAllUsers() {
+  const { rows } = await client.query("SELECT id, username, name, location, active FROM users;");
+
+  return rows;
+}
+
+async function createUser({ username, password, name, location }) {
+  try {
+    const rows = await client.query(
+      `
+      INSERT INTO users(username, password, name, location)
+      VALUES ($1, $2, $3, $4)
+      ON CONFLICT (username) DO NOTHING
+      RETURNING *;
+    `,
+      [username, password, name, location]
+    );
+
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  createUser,
+  client,
+  getAllUsers,
+};
